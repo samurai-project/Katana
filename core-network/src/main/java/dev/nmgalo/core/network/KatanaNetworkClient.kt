ef2ad4bf155f21de2cdd2e222ef824c605459482
nmgaloblishvili <=>
2022-07-21 18:36:17 +0400

    rename api client

--- core-network/src/main/java/dev/nmgalo/core/network/KatanaNetworkClient.kt
+++ core-network/src/main/java/dev/nmgalo/core/network/KatanaNetworkClient.kt
@@ -12,7 +12,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class ApiClient @Inject constructor(
+class KatanaNetworkClient @Inject constructor(
     json: Json
 ) : KatanaNetworkDataSource {
 
--- core-network/src/main/java/dev/nmgalo/core/network/di/NetworkModule.kt
+++ core-network/src/main/java/dev/nmgalo/core/network/di/NetworkModule.kt
@@ -5,7 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.nmgalo.core.network.ApiClient
+import dev.nmgalo.core.network.KatanaNetworkClient
 import dev.nmgalo.core.network.KatanaNetworkDataSource
 import kotlinx.serialization.json.Json
 import javax.inject.Singleton
@@ -15,7 +15,7 @@ import javax.inject.Singleton
 interface NetworkModule {
 
     @Binds
-    fun bindsKatanaNetwork(apiClient: ApiClient): KatanaNetworkDataSource
+    fun bindsKatanaNetwork(katanaNetworkClient: KatanaNetworkClient): KatanaNetworkDataSource
 
     companion object {
         @Provides
