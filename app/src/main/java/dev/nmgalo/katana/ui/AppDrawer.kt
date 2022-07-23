a5728266db73e59045040ec4a6710077a0f903e4
Nika <nikolozamgalo@gmail.com>
2022-07-23 15:08:43 +0400

    fix: wildcard import

--- app/src/main/java/dev/nmgalo/katana/ui/AppDrawer.kt
+++ app/src/main/java/dev/nmgalo/katana/ui/AppDrawer.kt
@@ -1,10 +1,20 @@
 package dev.nmgalo.katana.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
