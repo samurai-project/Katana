37d02897170832268bdd172275db1073e8926cb9
Nika mgaloblishvili <33604954+nmgalo@users.noreply.github.com>
2023-01-26 17:48:08 +0400

    rm extra navigation drawer

--- app/src/main/java/dev/nmgalo/katana/ui/KatanaApp.kt
+++ app/src/main/java/dev/nmgalo/katana/ui/KatanaApp.kt
@@ -1,103 +1,59 @@
 package dev.nmgalo.katana.ui
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Sms
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dev.nmgalo.feature.messenger.navigation.messengerGraph
 import dev.nmgalo.feature.wall.navigation.wallGraph
 import dev.nmgalo.katana.ui.theme.KatanaTheme
-import kotlinx.coroutines.launch
-
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Suppress("LongMethod")
 @Composable
 fun KatanaApp() {
 
     val navController = rememberNavController()
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route ?: "wall"
-    val scope = rememberCoroutineScope()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
 
     KatanaTheme {
-        ModalNavigationDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-                AppDrawer(
-                    currentRoute = currentRoute,
-                    navigateTo = { route -> navController.navigate(route) },
-                    closeDrawer = { scope.launch { drawerState.close() } },
-                    modifier = Modifier
-                        .statusBarsPadding()
-                        .navigationBarsPadding()
-                )
-            }
-        ) {
-            Scaffold(
-                topBar = {
-                    SmallTopAppBar(
-                        title = { Text("Wall") },
-                        navigationIcon = {
-                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
-                                Icon(
-                                    imageVector = Icons.Filled.Menu,
-                                    contentDescription = "Menu"
-                                )
+        Scaffold(
+            topBar = {
+                SmallTopAppBar(title = { Text("Wall") }, actions = {
+                    IconButton(onClick = {
+                        navController.navigate("messenger")
+                    }) {
+                        BadgedBox(badge = {
+                            Badge {
+                                Text(text = "3", color = Color.White)
                             }
-                        },
-                        actions = {
-                            IconButton(onClick = {
-                                navController.navigate("messenger")
-                            }) {
-                                BadgedBox(
-                                    badge = {
-                                        Badge {
-                                            Text(text = "3", color = Color.White)
-                                        }
-                                    }
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Sms,
-                                        contentDescription = "Chat"
-                                    )
-                                }
-                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Sms, contentDescription = "Chat"
+                            )
                         }
-                    )
-                },
-                containerColor = Color.Transparent,
-                contentColor = MaterialTheme.colorScheme.onBackground,
-            ) { padding ->
-                Row(modifier = Modifier.padding(padding)) {
-                    NavHost(navController = navController, startDestination = "wall") {
-                        wallGraph()
-                        messengerGraph()
                     }
+                })
+            },
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.onBackground,
+        ) { padding ->
+            Row(modifier = Modifier.padding(padding)) {
+                NavHost(navController = navController, startDestination = "wall") {
+                    wallGraph()
+                    messengerGraph()
                 }
             }
         }
