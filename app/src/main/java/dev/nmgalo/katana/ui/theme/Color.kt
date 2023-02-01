c6c9674a206fe9a367c595fad6158362169b8c52
Nika mgaloblishvili <33604954+nmgalo@users.noreply.github.com>
2023-02-02 00:04:27 +0400

    design system color updates, changed default theme

--- app/src/main/java/dev/nmgalo/katana/ui/theme/Color.kt
+++ app/src/main/java/dev/nmgalo/katana/ui/theme/Color.kt
@@ -1,8 +1,44 @@
+@file:Suppress("TopLevelPropertyNaming")
+
 package dev.nmgalo.katana.ui.theme
 
-const val RED_200 = 0xfff297a2
-const val RED_300 = 0xffea6d7e
-const val RED_700 = 0xffdd0d3c
-const val RED_800 = 0xffd00036
-const val RED_900 = 0xffc20029
+const val Blue10 = 0xFF000F5E
+const val Blue20 = 0xFF001E92
+const val Blue30 = 0xFF002ECC
+const val Blue40 = 0xFF1546F6
+const val Blue80 = 0xFFB8C3FF
+const val Blue90 = 0xFFDDE1FF
 
+const val DarkBlue10 = 0xFF00036B
+const val DarkBlue20 = 0xFF000BA6
+const val DarkBlue30 = 0xFF1026D3
+const val DarkBlue40 = 0xFF3648EA
+const val DarkBlue80 = 0xFFBBC2FF
+const val DarkBlue90 = 0xFFDEE0FF
+
+const val Yellow10 = 0xFF261900
+const val Yellow20 = 0xFF402D00
+const val Yellow30 = 0xFF5C4200
+const val Yellow40 = 0xFF7A5900
+const val Yellow80 = 0xFFFABD1B
+const val Yellow90 = 0xFFFFDE9C
+
+const val Red10 = 0xFF410001
+const val Red20 = 0xFF680003
+const val Red30 = 0xFF930006
+const val Red40 = 0xFFBA1B1B
+const val Red80 = 0xFFFFB4A9
+const val Red90 = 0xFFFFDAD4
+
+const val Grey10 = 0xFF191C1D
+const val Grey20 = 0xFF2D3132
+const val Grey80 = 0xFFC4C7C7
+const val Grey90 = 0xFFE0E3E3
+const val Grey95 = 0xFFEFF1F1
+const val Grey99 = 0xFFFBFDFD
+
+const val BlueGrey30 = 0xFF45464F
+const val BlueGrey50 = 0xFF767680
+const val BlueGrey60 = 0xFF90909A
+const val BlueGrey80 = 0xFFC6C5D0
+const val BlueGrey90 = 0xFFE2E1EC
