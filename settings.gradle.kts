68228f84b77a644bae4ee9044f706a08380c57b4
Nika mgaloblishvili <33604954+nmgalo@users.noreply.github.com>
2023-02-05 15:02:31 +0400

    gradle build code cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,16 +10,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven(url = "https://androidx.dev/snapshots/builds/8455591/artifacts/repository") {
-            content {
-                includeGroupByRegex("androidx\\..*")
-            }
-        }
         google()
         mavenCentral()
     }
 }
-
 
 rootProject.name = "Katana"
 include(":app")
