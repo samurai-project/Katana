245e7f85650b631a780966072bb99da8ccd00361
Nika mgaloblishvili <nikolozamgalo@gmail.com>
2023-02-10 23:10:25 +0400

    remove extra preference helper from viewModel

--- app/src/main/java/dev/nmgalo/katana/ui/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/dev/nmgalo/katana/ui/onboarding/OnBoardingScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowRightAlt
@@ -76,6 +77,7 @@ fun OnBoardingPagerItem(page: Int) {
     val item = OnBoardingTab.values()[page]
     Column(modifier = Modifier.padding(20.dp)) {
         Image(
+            modifier = Modifier.size(200.dp),
             painter = painterResource(item.image),
             contentDescription = "OnBoarding Image"
         )
